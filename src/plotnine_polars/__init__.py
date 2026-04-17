1804acfc40eb55e3e5a3b07a27cc55ece0036d6d
Ian Gow <iandgow@gmail.com>
2026-04-17 19:09:06 -0400

    feat: re-export plotnine public API; bump to 0.2.0
    Allows users to import aes, element_text, etc. directly from
    plotnine_polars, removing the need for a separate plotnine import.

--- src/plotnine_polars/__init__.py
+++ src/plotnine_polars/__init__.py
@@ -1,4 +1,6 @@
+from plotnine import *  # noqa: F401, F403
+from plotnine import __all__ as _plotnine_all
 from ._fluent import ggplot
 from . import _polars_namespace as _polars_namespace
 
-__all__ = ("ggplot",)
+__all__ = ("ggplot", *_plotnine_all)
